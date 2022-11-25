0dc373a9b42dfd4a7dc62128631b0e57c070536d
Anton Morozov <anomorch@gmail.com>
2022-11-25 13:10:00 +0300

    change the layout

--- login/authorization.js
+++ login/authorization.js
@@ -4,15 +4,17 @@ const username = document.getElementById('name');
 const password = document.getElementById('password');
 const errorElement = document.getElementById('error');
 
+const pathIfRequestIsSuccessful = '../game/index.html';
+
 form.addEventListener('submit', (e) => {
     let errorMessage = null;
     e.preventDefault();
 
     if (localStorage.getItem(username.value) == password.value) {
-        window.location.href = 'game.html';
+        window.location.href = pathIfRequestIsSuccessful;
     } else if (localStorage.getItem(username.value) == null) {
         localStorage.setItem(`${username.value}`, `${password.value}`);
-        window.location.href = 'game.html';
+        window.location.href = pathIfRequestIsSuccessful;
     } else {
         errorMessage = 'Неверный пароль!';
         errorElement.innerText = errorMessage;
