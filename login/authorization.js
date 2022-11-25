98f7e612c6e96fa879be78f795d53b7a374900de
Anton Morozov <anomorch@gmail.com>
2022-11-25 15:37:46 +0300

    style the login page

--- login/authorization.js
+++ login/authorization.js
@@ -7,16 +7,15 @@ const errorElement = document.getElementById('error');
 const pathIfRequestIsSuccessful = '../game/index.html';
 
 form.addEventListener('submit', (e) => {
-    let errorMessage = null;
-    e.preventDefault();
-
     if (localStorage.getItem(username.value) == password.value) {
         window.location.href = pathIfRequestIsSuccessful;
     } else if (localStorage.getItem(username.value) == null) {
         localStorage.setItem(`${username.value}`, `${password.value}`);
         window.location.href = pathIfRequestIsSuccessful;
     } else {
-        errorMessage = 'Неверный пароль!';
-        errorElement.innerText = errorMessage;
+        e.preventDefault();
+        errorElement.innerHTML = 
+            '<span style="font-weight: bold;">Ошибка!</span>' +
+            '<br> Неверный пароль';
     }
 });
