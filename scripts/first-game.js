ed08a32c8756a95a545e0f17c36c4d63b04c6e67
Anton Morozov <anomorch@gmail.com>
2022-11-27 11:40:55 +0300

    update the first game script

--- scripts/first-game.js
+++ scripts/first-game.js
@@ -1,37 +1,48 @@
-const colors = [
-    'rgba(250, 69, 69, 0.8)',
-    'rgba(250, 155, 69, 0.8)',
-    'rgba(250, 239, 69, 0.8)',
-    'rgba(110, 250, 69, 0.8)',
-    'rgba(69, 246, 250, 0.8)',
-    'rgba(69, 121, 250, 0.8)',
-    'rgba(197, 69, 250, 0.8)'
-]
+const colorsDict = {
+    'красный': 'rgba(250, 69, 69, 0.8)',
+    'оранжевый': 'rgba(250, 155, 69, 0.8)',
+    'желтый': 'rgba(250, 239, 69, 0.8)',
+    'зеленый': 'rgba(110, 250, 69, 0.8)',
+    'голубой': 'rgba(69, 246, 250, 0.8)',
+    'синий': 'rgba(69, 121, 250, 0.8)',
+    'фиолетовый': 'rgba(197, 69, 250, 0.8)'
+}
 
-const colorsName = [
-    'КРАСНЫЙ',
-    'ОРАНЖЕВЫЙ',
-    'ЖЕЛТЫЙ',
-    'ЗЕЛЕНЫЙ',
-    'ГОЛУБОЙ',
-    'СИНИЙ',
-    'ФИОЛЕТОВЫЙ'
-]
+const colorsDictLength = Object.keys(colorsDict).length;
+
+const counter = document.getElementById('first-counter');
+let count = 0;
+counter.innerText = count;
+
+const pointsForCorrectAnswer = 5;
+const pointsForWrongAnswer = -2;
 
 const requiredColorsAmount = 3;
 let randomIndexes = []
 
-for (i = 0; i < requiredColorsAmount; i++) {
-    randomIndexes.push(getNonRepeatedRandomInt(0, colors.length, randomIndexes));
-}
-
 const playbox = document.getElementById('first-game');
-playbox.style.backgroundColor = colors[randomIndexes[0]];
-
 const playboxWord = document.getElementById('first-game-word');
-playboxWord.style.color = colors[randomIndexes[1]];
 
-playboxWord.innerText = colorsName[randomIndexes[2]];
+resetColors();
+
+const choices = document.getElementsByName('colors');
+const form = document.getElementById('first-conditions');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    choices.forEach((choice) => {
+        if (choice.checked) {
+            if (isAnswerCorrect(choice)) {
+                count += pointsForCorrectAnswer;
+            } else {
+                count += pointsForWrongAnswer;
+            }
+
+            counter.innerText = count;
+            resetColors();
+        }
+    });
+});
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
@@ -47,4 +58,22 @@ function getNonRepeatedRandomInt(min, max, usedColorIndexes) {
     }
 
     return index;
+}
+
+function isAnswerCorrect(answer) {
+    const correctAnswer = playboxWord.style.color;
+    answer = colorsDict[answer.value.toLowerCase()];
+    return correctAnswer == answer;
+}
+
+function resetColors() {
+    randomIndexes.splice(0, randomIndexes.length);
+
+    for (i = 0; i < requiredColorsAmount; i++) {
+        randomIndexes.push(getNonRepeatedRandomInt(0, colorsDictLength, randomIndexes));
+    }
+
+    playbox.style.backgroundColor = Object.values(colorsDict)[randomIndexes[0]];
+    playboxWord.style.color = Object.values(colorsDict)[randomIndexes[1]];
+    playboxWord.innerText = Object.keys(colorsDict)[randomIndexes[2]].toUpperCase();
 }
