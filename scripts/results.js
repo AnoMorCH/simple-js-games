4c8c785d470e6aab752df96ea5e6a3c8ed938b68
Anton Morozov <anomorch@gmail.com>
2022-11-30 15:26:17 +0300

    add a script for the result page wo styles

--- scripts/results.js
+++ scripts/results.js
@@ -1,6 +1,53 @@
+let localStorageObject = Object.entries(localStorage);
+
+let currentUser = null;
+let currentUserId = -1;
+updateCurrentUserAndHisId();
+
+const userName = currentUser[0];
+const userPassword = currentUser[1];
+
 const finishGameBtn = document.getElementById('third-move-in-form');
+const ratingPlaceholder = document.getElementById('rating-placeholder');
 
 finishGameBtn.addEventListener('click', () => {
-    const result1 = document.getElementById('result-1');
-    result1.innerText = globalCount;
+    parseLocalStorage();
+    addUsersToPlaceholder();
 });
+
+function updateCurrentUserAndHisId() {
+    while (
+        currentUser == null &&
+        currentUserId < localStorageObject.length - 1
+    ) {
+        currentUserId += 1;
+
+        try {
+            JSON.parse(userData);
+        } catch {
+            currentUser = localStorageObject[currentUserId];
+        }
+    }
+}
+
+function parseLocalStorage() {
+    const userData = {
+        'password': userPassword,
+        'score': globalCount
+    }
+
+    const jsonUserData = JSON.stringify(userData);
+
+    localStorage.removeItem(userName);
+    localStorage.setItem(userName, jsonUserData);
+    localStorageObject = Object.entries(localStorage);
+}
+
+function addUsersToPlaceholder() {
+    for (const [userName, userData] of localStorageObject) {
+        const li = document.createElement('li');
+        const userScore = JSON.parse(userData)['score'];
+        li.innerHTML = `${userName} - ${userScore}`;
+        ratingPlaceholder.appendChild(li);
+    }
+}
